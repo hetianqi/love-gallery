2ae607d4345058a3e913a458c914f396b2584603
hetianqi <449165578@qq.com>
2018-04-04 20:23:17 +0800

    完成图片布局及操作

---
+++ build/webpack.config.dev.js
@@ -0,0 +1,87 @@
+/**
+ * webpack 开发模式配置
+ */
+
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+function join(p) {
+	return path.join(__dirname, p);
+}
+
+module.exports = {
+	entry: {
+		app: join('../src/index.jsx'),
+		vendor: ['react', 'react-dom', 'babel-polyfill']
+	},
+	output: {
+		path: join('../dist'),
+		filename: '[name].js',
+		hashDigestLength: 6
+	},
+	resolve: {
+		extensions: ['.js', '.jsx', '*']
+	},
+	module: {
+		rules: [{
+			test: /\.jsx?$/,
+			use: 'babel-loader',
+			exclude: /node_modules/
+		},
+		{
+			test: /\.less$/,
+			use: [
+				'style-loader', 
+				'css-loader', 
+				'postcss-loader',
+				'less-loader'
+			]
+		},
+		{
+			test: /\.(png|gif|jpg|jpeg|bmp)$/i,
+			use : {
+				loader : 'url-loader',
+				options : {
+					limit : '8192'
+				}
+			}
+		},
+		{
+			test: /\.(woff|woff2|svg|ttf|eot)($|\?)/i,
+			use: {
+				loader: 'url-loader',
+				options: {
+					limit: '8192'
+				}
+			}
+		}]
+	},
+	plugins: [
+		new webpack.DefinePlugin({ 
+			'process.env': {
+				NODE_ENV: JSON.stringify('development') // 环境定义
+			}
+		}),
+		new HtmlWebpackPlugin({
+			filename: path.join(__dirname, '../dist/index.html'),
+			template: path.join(__dirname, '../src/index.html')
+		}),
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'vendor',
+			minChunks: Infinity
+		}),
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'manifest'
+		})
+	],
+	devServer: {
+		contentBase: path.join(__dirname, '../dist'),
+		port: 8000,
+		hot: true,
+		compress: true,
+		host: '0.0.0.0',
+		public: 'localhost:8000'
+	}
+};
